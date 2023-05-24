3ddec37fb354a23827e5f905e4903c0070df1d39
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-24 19:06:30 +0200

    persis save location state in browser storage

--- client/src/routes/Locations/Locations.jsx
+++ client/src/routes/Locations/Locations.jsx
@@ -146,8 +146,33 @@ export const LocationsHeader = () => {
   );
 };
 
+const useLocalStorage = (key, initialValue) => {
+  const [state, setState] = useState(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : initialValue;
+    } catch (error) {
+      console.error("Error retrieving data from local storage:", error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      localStorage.setItem(key, JSON.stringify(state));
+    } catch (error) {
+      console.error("Error storing data in local storage:", error);
+    }
+  }, [key, state]);
+
+  return [state, setState];
+};
+
 const Locations = () => {
-  const [savedLocations, setSavedLocations] = useState(["belgrade"]);
+  const [savedLocations, setSavedLocations] = useLocalStorage(
+    "saveLocations",
+    []
+  );
   return (
     <div className="px-7 max-w-3xl m-auto">
       <LocationsHeader />
