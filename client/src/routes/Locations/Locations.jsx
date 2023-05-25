1880349487d0ea688b948f148975efb2bd860fb3
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-05-25 18:46:37 +0200

    added location grid responsive class names

--- client/src/routes/Locations/Locations.jsx
+++ client/src/routes/Locations/Locations.jsx
@@ -242,7 +242,7 @@ export const LocationsGridList = ({
   handleRemove,
 }) => {
   return (
-    <section className="grid grid-cols-2 gap-4 sm:grid-cols-3">
+    <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
       {data.map((location) => {
         return (
           <LocationItem
