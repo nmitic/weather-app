ede8e7a88a0ee3cfa712906d465e5f720bae5162
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-01 23:23:04 +0200

    created skeleton view and use it on root

--- client/src/routes/Root/Root.jsx
+++ client/src/routes/Root/Root.jsx
@@ -61,14 +61,83 @@ const Error = () => {
   );
 };
 
+const SkeletonWeather = () => {
+  return (
+    <div role="status" class="animate-pulse mb-5">
+      <div className="flex justify-between">
+        <div class=" h-8 w-20 bg-gray-300 rounded-lg"></div>
+        <div class=" h-8 w-8 bg-gray-300 rounded-lg"></div>
+      </div>
+      <div className="flex flex-col items-center py-36">
+        <div class="h-32 w-32 bg-gray-300 rounded-lg mb-4"></div>
+        <div class="h-20 w-20 bg-gray-300 rounded-lg mb-4"></div>
+        <div class="h-8 w-20 bg-gray-300 rounded-lg mb-4"></div>
+      </div>
+      <div class=" h-16 w-full bg-gray-300 rounded-lg"></div>
+    </div>
+  );
+};
+
+const SkeletonForecast = () => {
+  return (
+    <div role="status" class="animate-pulse">
+      <div className="flex justify-around flex-wrap">
+        {[...new Array(8)].map(() => {
+          return (
+            <div className="flex items-center flex-col mb-4">
+              <div className=" h-4 w-20 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-8 w-8 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-6 w-6 bg-gray-300 rounded-lg"></div>
+            </div>
+          );
+        })}
+        {[...new Array(8)].map(() => {
+          return (
+            <div className="flex items-center flex-col mb-4">
+              <div className=" h-4 w-20 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-8 w-8 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-6 w-6 bg-gray-300 rounded-lg"></div>
+            </div>
+          );
+        })}
+        {[...new Array(8)].map(() => {
+          return (
+            <div className="flex items-center flex-col mb-4">
+              <div className=" h-4 w-20 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-8 w-8 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-6 w-6 bg-gray-300 rounded-lg"></div>
+            </div>
+          );
+        })}
+        {[...new Array(8)].map(() => {
+          return (
+            <div className="flex items-center flex-col mb-4">
+              <div className=" h-4 w-20 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-8 w-8 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-6 w-6 bg-gray-300 rounded-lg"></div>
+            </div>
+          );
+        })}
+        {[...new Array(8)].map(() => {
+          return (
+            <div className="flex items-center flex-col mb-4">
+              <div className=" h-4 w-20 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-8 w-8 bg-gray-300 rounded-lg mb-4"></div>
+              <div className=" h-6 w-6 bg-gray-300 rounded-lg"></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 const Root = () => {
   const data = useLoaderData();
 
   return (
     <div className="px-7 max-w-3xl m-auto">
-      <React.Suspense
-        fallback={<p>Loading current weather location data...</p>}
-      >
+      <React.Suspense fallback={<SkeletonWeather />}>
         <Await resolve={data.currentWeather} errorElement={<Error />}>
           {(currentWeather) => (
             <LocationWeatherView weatherData={currentWeather} />
@@ -76,7 +145,7 @@ const Root = () => {
         </Await>
       </React.Suspense>
 
-      <React.Suspense fallback={<p>Loading forecastWeather...</p>}>
+      <React.Suspense fallback={<SkeletonForecast />}>
         <Await resolve={data.forecastWeather} errorElement={<Error />}>
           {(forecastWeather) => <Forecast forecastData={forecastWeather} />}
         </Await>
