3e9755774a7eaf1bf98890e6544804c8c1efc82c
Nikola Mitic <nikola.mitic.dev@gmail.com>
2023-06-02 16:36:17 +0200

    fixed missing key prop

--- client/src/components/LocationWeatherView/components/Forecast/Forecast.jsx
+++ client/src/components/LocationWeatherView/components/Forecast/Forecast.jsx
@@ -15,12 +15,12 @@ export const Forecast = ({ forecastData }) => {
 export const SkeletonForecast = () => {
   return (
     <div role="status" className="animate-pulse">
-      {[...new Array(5)].map(() => {
+      {[...new Array(5).keys()].map((item) => {
         return (
-          <div className="flex text-center overflow-scroll mb-16">
-            {[...new Array(8)].map(() => {
+          <div className="flex text-center overflow-scroll mb-16" key={item}>
+            {[...new Array(8).keys()].map((item) => {
               return (
-                <div className="mr-2">
+                <div className="mr-2" key={item}>
                   <div className="h-4 w-20 bg-gray-300 rounded-lg mb-4 mx-auto"></div>
                   <div className="h-8 w-8 bg-gray-300 rounded-lg mb-4 mx-auto"></div>
                   <div className="h-6 w-6 bg-gray-300 rounded-lg mx-auto"></div>
